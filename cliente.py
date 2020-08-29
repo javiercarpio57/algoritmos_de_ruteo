81c06ec79393ec142f295bfa9b48dfb8e93cbad2
gusta257 <gdeleontobias@gmail.com>
2020-08-29 17:49:29 -0600

    avances

--- cliente.py
+++ cliente.py
@@ -9,11 +9,14 @@ def connect():
 @sio.event
 def my_message(data):
     print('message received with ', data)
-    sio.emit('my response', {'response': 'my response'})
+    sio.emit('message', data)
 
 @sio.event
 def disconnect():
     print('disconnected from server')
 
 sio.connect('http://localhost:5000')
+
+my_message("holaaaa")
+
 sio.wait()
--- server.py
+++ server.py
@@ -1,26 +1,43 @@
-import socketio
 import eventlet
+import socketio
+from utilities import *
+import threading
 
-# create a Socket.IO server
+
+global nodos
+global usuarios
+nodos = int(input("Ingrese el numero de nodos: "))
+usuarios = 0
 sio = socketio.Server()
 app = socketio.WSGIApp(sio, static_files={
     '/': {'content_type': 'text/html', 'filename': 'index.html'}
 })
 
-
 @sio.event
-def connect(sid,environ):
-    print('Se ha conectado',sid)
-    my_message(sid, 'funciono')
+def connect(sid, environ):
+    global usuarios
+    usuarios +=1
+    
+    print('connect ', sid)
+    print("Se han conectado",usuarios,"usuarios faltan",nodos-usuarios)
+    if(nodos-usuario == 0):
+        pesos = get_weights("weights.csv")
+        nodos = get_nodes(pesos)
+        
+        sio.emit()
+@sio.event
+def message(sid, data):
+    print("entro")
+    print('message ', data)
 
 @sio.event
 def disconnect(sid):
-    print('Se ha desconectado', sid)
+    print('disconnect ', sid)
 
-@sio.event
-def my_message(sid, data):
-    sio.emit('Test', data)
-    print('Send message ', data)
 
 if __name__ == '__main__':
+
     eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+    
+
+
